e876eed74f3927a850d88561cf05a1e5bf99b21f
Samir Rashid <Samir-Rashid@godsped.com>
2026-05-17 22:46:05 -0700

    serve js and css

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import maplibregl from 'maplibre-gl';
+import CompassControl from '@mapbox-controls/compass';
+import RulerControl from '@mapbox-controls/ruler';
+
+const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/streets-v4/style.json?key=4F7xYJRpkPdD3qSM9UoZ',
+    center: [-122.420679, 37.772537], // San Francisco
+    zoom: 12
+});
+
+// Add Compass Control
+map.addControl(new CompassControl(), 'bottom-right');
+
+// Add Ruler Control
+map.addControl(new RulerControl(), 'bottom-right');
+
+// Ruler Events
+map.on('ruler.on', () => console.log('Ruler activated'));
+map.on('ruler.off', () => console.log('Ruler deactivated'));
+
