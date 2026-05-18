e56f6894f673d5f55acc857e53c71e2ee002128e
Samir Rashid <Samir-Rashid@godsped.com>
2026-05-17 18:16:20 -0700

    quick start map
    from here https://maplibre.org/maplibre-gl-js/docs/
    
    This repo is already too overcomplicated with bun `bun run index.ts` for a static site

---
+++ map.ts
@@ -0,0 +1,8 @@
+import maplibregl from 'maplibre-gl';
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/globe.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+});
