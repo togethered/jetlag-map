815da51eeaca5cb4f6c4de67f85e2a31dcf2cd91
Samir Rashid <Samir-Rashid@godsped.com>
2026-05-17 19:24:43 -0700

    improve map

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import index from './index.html';
+import index from './src/index.html';
 
 const server = Bun.serve({
   port: 3000,
--- map.ts
+++ map.ts
@@ -3,6 +3,6 @@ import maplibregl from 'maplibre-gl';
 const map = new maplibregl.Map({
     container: 'map', // container id
     style: 'https://demotiles.maplibre.org/globe.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
+    center: [37.452520, 122.265640], // starting position [lng, lat]
     zoom: 1 // starting zoom
 });
