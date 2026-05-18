cc95fcdee51867fb17ed468b8627ab7c550b6604
Samir Rashid <Samir-Rashid@godsped.com>
2026-05-17 17:22:07 -0700

    bun

---
+++ index.ts
@@ -0,0 +1,14 @@
+import index from './index.html';
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": index, 
+    // "/figlet": () => {
+    //   const body = figlet.textSync('Bun!');
+    //   return new Response(body);
+    // }
+  }
+});
+
+console.log(`Listening on ${server.url}`);
